ff9e88fa73ac3404a128eeaf2fad4a6743c337e5
joygachuhi <joygachuhi33@gmail.com>
2026-02-19 16:07:44 +0300

    Add files via upload

---
+++ FolderDSA/Linkedlists/SinglyLinkedLists/implementation/song.java
@@ -0,0 +1,16 @@
+package LinkedList.java;
+
+public class song {
+	String title;
+	String artist;
+	int duration;
+	song(String title,String artist,int duration){ 
+		this.title = title;
+		this.artist = artist;
+		this.duration = duration;
+	}
+	public song(String title2, String artist2, int duration2) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
